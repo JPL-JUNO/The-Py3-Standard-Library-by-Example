89ff3ac6d4e909bb85cb9f1447d283abdf619577
Stephen CUI <junocuixuan@163.com>
2026-01-18 16:02:26 +0800

    从文件系统树最顶层的完整路径

--- 06FileSystem/ospath/abspath.py
+++ 06FileSystem/ospath/abspath.py
@@ -3,7 +3,7 @@ Created time: 2026-01-11 22:40:40
 Author(s)   : Stephen CUI
 File        : abspath.py
 Email       : cuixuanstephen@gmail.com
-Description :
+Description : 从文件系统树最顶层的完整路径
 """
 
 import os
